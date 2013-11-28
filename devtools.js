573d7610036c8d6c5901d15abde793160278370e
James Phillpotts <jphillpotts@scottlogic.co.uk>
2013-11-28 10:40:48 +0000

    JSHint on save

---
+++ devtools.js
@@ -0,0 +1,21 @@
+
+function validateScript(content, url) {
+  var isValid = JSHINT(content, {});
+  if (!isValid) {
+    JSHINT.errors.forEach(function(error) {
+      if (error === null) {
+        // Why does JSHINT return a null terminated array?
+        return;
+      }
+      chrome.experimental.devtools.console.addMessage(chrome.experimental.devtools.console.Severity.Error, error.reason, url, error.line);
+    });
+  }
+}
+
+chrome.devtools.inspectedWindow.onResourceContentCommitted.addListener(function(resource, content) {
+  var url = resource.url;
+  if (url.lastIndexOf(".js") === url.length - 3) {
+    validateScript(content, resource.url);
+  }
+});
+
